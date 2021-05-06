8648f617021562524ed593e8d88bf8407c09517f
Catherine Li <catherine.uk.20@gmail.com>
2021-05-06 23:47:36 +0100

    Challenge done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 console.log(data);
 // We create individual components here
 
-// DOG LIST BUTTON
 function addDogListButton(dog){
     let dogList = document.querySelector(".dogs-list")
     let button = document.createElement("li")
@@ -95,9 +94,113 @@ function selectGoodBadDog(){
    }
 }
 
+
+function addNewDog(){
+    let cleanContent = document.querySelector(".main__dog-section")
+    cleanContent.remove()
+
+    let main = document.querySelector(".main")
+
+    let mainDogSection = document.createElement("section")
+    mainDogSection.setAttribute("class","main__dog-section")
+
+    let h2AddDog = document.createElement("h2")
+    h2AddDog.innerText = "Add a new Dog"
+
+    let formEl = document.createElement("form")
+    formEl.setAttribute("class","form")
+
+    let nameLabel = document.createElement("label")
+    nameLabel.setAttribute("for","name")
+    nameLabel.innerText = "Dog's Name"
+    let nameInput = document.createElement("input")
+    nameInput.setAttribute("type","text")
+    nameInput.setAttribute("id","name")
+    nameInput.setAttribute("name","name")
+
+    let imgLabel = document.createElement("label")
+    imgLabel.setAttribute("for","image")
+    imgLabel.innerText = "Dog's Picture"
+    let imgInput = document.createElement("input")
+    imgInput.setAttribute("type","url")
+    imgInput.setAttribute("id","image")
+    imgInput.setAttribute("name","image")
+
+    let bioLabel = document.createElement("label")
+    bioLabel.setAttribute("for","bio")
+    bioLabel.innerText = "Dog's bio"
+    let bioInput = document.createElement("textarea")
+    bioInput.setAttribute("row","5")
+    bioInput.setAttribute("id","bio")
+    bioInput.setAttribute("name","bio")
+
+    let formButton = document.createElement("input")
+    formButton.setAttribute("type","submit")
+    formButton.setAttribute("id","submit")
+    formButton.setAttribute("name","submit")
+    formButton.setAttribute("value","Let's add a dog!")
+    formButton.setAttribute("class","form__button")
+
+    main.append(mainDogSection)
+    mainDogSection.append(h2AddDog, formEl)
+    formEl.append(nameLabel, nameInput, imgLabel, imgInput, bioLabel, bioInput, formButton)
+
+    formEl.addEventListener("submit",function (event){
+        event.preventDefault();
+    
+        let dogList = document.querySelector(".dogs-list")
+        let plusButton = document.querySelector(".dogs-list__button--add")
+        let button = document.createElement("li")
+        button.setAttribute("class", "dogs-list__button")
+        button.setAttribute("id", "newDog" )
+        button.innerText = formEl.name.value
+    
+        button.addEventListener("click",function(){
+            let cleanContent = document.querySelector(".main__dog-section")
+            cleanContent.remove()
+
+            let main = document.querySelector(".main")
+            let mainDogSection = document.createElement("section")
+            mainDogSection.setAttribute("class", "main__dog-section")
+
+            main.append(mainDogSection)
+
+            let h2Name = document.createElement("h2")
+            h2Name.innerText = formEl.name.value
+
+            let dogImageLink = formEl.image.value
+
+            let dogImage = document.createElement("img")
+            dogImage.setAttribute("style", "width:400px; height:300px")
+            dogImage.setAttribute("src", dogImageLink)
+
+            // main__dog-section__desc
+            let dogDesc = document.createElement("div")
+            dogDesc.setAttribute("class", "main__dog-section__desc")
+
+            let h3Bio = document.createElement("h3")
+            h3Bio.innerText = "Bio"
+
+            let bioContent = document.createElement("p")
+            bioContent.innerText = formEl.bio.value
+
+            mainDogSection.append(h2Name, dogImage, dogDesc)
+            dogDesc.append(h3Bio, bioContent)
+        })
+
+
+        plusButton.after(button)
+    })
+
+}
+
+
 // Generate the page
 
 for (dog of data){
     addDogListButton(dog)
 }
+
+let plusButton = document.querySelector(".dogs-list__button--add")
+plusButton.addEventListener("click", addNewDog)
 
